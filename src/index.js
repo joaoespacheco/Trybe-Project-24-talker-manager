44b9381790bc78060989442f04bb6ddf56091f39
joao emanuel soares pacheco <joaoe.pacheco@gmail.com>
2022-10-04 15:35:15 -0300

    Requisito 01 - Conclusão do requisito

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 const express = require('express');
+const fs = require('fs').promises;
+const path = require('path');
 const bodyParser = require('body-parser');
 
 const app = express();
@@ -10,6 +12,12 @@ const PORT = '3000';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+app.get('/talker', async (req, res) => {
+  const pathSpeakers = path.resolve(__dirname, 'talker.json');
+  const speakers = JSON.parse(await fs.readFile(pathSpeakers, 'utf8'));
+  res.status(200).json(speakers);
 });
 
 app.listen(PORT, () => {
